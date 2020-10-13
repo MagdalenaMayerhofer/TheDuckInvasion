d71f66e31b65fd358dd6a32df600975bb7a690a2
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2020-10-13 13:34:43 +0200

    add some prefabs, add creation of rubberducks

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public GameObject rubberDuckPrefab;
+    float creationIntervall = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Utils.CreateRubberDuckInstance(rubberDuckPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        creationIntervall--;
+
+        if (creationIntervall == 0)
+        {
+            creationIntervall = 20;
+            Utils.CreateRubberDuckInstance(rubberDuckPrefab);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Utils: MonoBehaviour
+{
+    public static void CreateRubberDuckInstance(GameObject rubberDuckPrefab)
+    {
+        var rightBorder = Camera.main.ViewportToWorldPoint( new Vector3(1, 0, 0)).x;
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+
+        Vector3 position = new Vector3(rightBorder, Random.Range(topBorder, bottomBorder), 0);
+        Instantiate(rubberDuckPrefab, position, Quaternion.identity);
+    }
+}
