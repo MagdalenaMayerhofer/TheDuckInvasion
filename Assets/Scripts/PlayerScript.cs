567c4171d207f074fe56994ae18094fed904ada5
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2020-10-13 21:32:00 +0200

    add snowball attack

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -26,17 +26,24 @@ public class PlayerScript : MonoBehaviour
           speed.y * inputY);
 
         // 5 - Shooting
-        bool shoot = Input.GetButtonDown("Fire1");
-        shoot |= Input.GetButtonDown("Fire2");
+        bool bread = Input.GetButtonDown("Fire1");
+        bool snowball = Input.GetButtonDown("Fire2");
         // Careful: For Mac users, ctrl + arrow is a bad idea
 
-        if (shoot)
+        if (bread)
         {
             WeaponScript weapon = GetComponent<WeaponScript>();
             if (weapon != null)
             {
-                // false because the player is not an enemy
-                weapon.Attack();
+                weapon.FoodAttack();
+                SoundEffectsHelper.Instance.MakePlayerShotSound();
+            }
+        } else if (snowball)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                weapon.IceAttack();
                 SoundEffectsHelper.Instance.MakePlayerShotSound();
             }
         }
@@ -44,29 +51,19 @@ public class PlayerScript : MonoBehaviour
         // 6 - Make sure we are not outside the camera bounds
         var dist = (transform.position - Camera.main.transform.position).z;
 
-        var leftBorder = Camera.main.ViewportToWorldPoint(
-          new Vector3(0, 0, dist)
-        ).x;
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
 
-        var rightBorder = Camera.main.ViewportToWorldPoint(
-          new Vector3(1, 0, dist)
-        ).x;
+        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
 
-        var topBorder = Camera.main.ViewportToWorldPoint(
-          new Vector3(0, 0, dist)
-        ).y;
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
 
-        var bottomBorder = Camera.main.ViewportToWorldPoint(
-          new Vector3(0, 1, dist)
-        ).y;
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
 
         transform.position = new Vector3(
           Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
           Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
           transform.position.z
         );
-
-
     }
 
     void FixedUpdate()
--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -12,31 +12,9 @@ public class WeaponScript : MonoBehaviour
     /// <summary>
     /// Projectile prefab for shooting
     /// </summary>
-    public Transform shotPrefab;
+    public Transform foodPrefab;
+    public Transform icePrefab;
 
-    /// <summary>
-    /// Cooldown in seconds between two shots
-    /// </summary>
-    public float shootingRate = 0.25f;
-
-    //--------------------------------
-    // 2 - Cooldown
-    //--------------------------------
-
-    private float shootCooldown;
-
-    void Start()
-    {
-        shootCooldown = 0f;
-    }
-
-    void Update()
-    {
-        if (shootCooldown > 0)
-        {
-            shootCooldown -= Time.deltaTime;
-        }
-    }
 
     //--------------------------------
     // 3 - Shooting from another script
@@ -45,38 +23,41 @@ public class WeaponScript : MonoBehaviour
     /// <summary>
     /// Create a new projectile if possible
     /// </summary>
-    public void Attack()
+    public void FoodAttack()
     {
-        if (CanAttack)
+        // Create a new shot
+        var shotTransform = Instantiate(foodPrefab);
+
+        // Assign position
+        shotTransform.position = transform.position;
+
+        // The is enemy property
+        ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+
+        // Make the weapon shot always towards it
+        MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+        if (move != null)
         {
-            shootCooldown = shootingRate;
-
-            // Create a new shot
-            var shotTransform = Instantiate(shotPrefab) as Transform;
-
-            // Assign position
-            shotTransform.position = transform.position;
-
-            // The is enemy property
-            ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
-
-            // Make the weapon shot always towards it
-            MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
-            if (move != null)
-            {
-                move.direction = this.transform.right; // towards in 2D space is the right of the sprite
-            }
+            move.direction = this.transform.right; // towards in 2D space is the right of the sprite
         }
     }
 
-    /// <summary>
-    /// Is the weapon ready to create a new projectile?
-    /// </summary>
-    public bool CanAttack
+    public void IceAttack()
     {
-        get
+        // Create a new shot
+        var shotTransform = Instantiate(icePrefab);
+
+        // Assign position
+        shotTransform.position = transform.position;
+
+        // The is enemy property
+        ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+
+        // Make the weapon shot always towards it
+        MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+        if (move != null)
         {
-            return shootCooldown <= 0f;
+            move.direction = this.transform.right; // towards in 2D space is the right of the sprite
         }
     }
 }
