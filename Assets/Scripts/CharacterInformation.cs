89d02b86a4d47837d657812cda68e2040ee836c4
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2020-10-14 21:09:38 +0200

    add player

--- Assets/Scripts/CharacterInformation.cs
+++ Assets/Scripts/CharacterInformation.cs
@@ -10,5 +10,4 @@ public static class CharacterInformation
     {
         Grandma, Student
     }
-
 }
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject grandmaPrefab;
+    public GameObject studentPrefab;
+
+    GameObject chosenCharacterPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetPlayerCharacter();
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0.08f, 0, 0)).x;
+        var verticalMiddle = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).y;
+
+        Vector3 position = new Vector3(leftBorder, verticalMiddle, 0);
+        Instantiate(chosenCharacterPrefab, position, Quaternion.identity);
+    }
+
+    public void GetPlayerCharacter()
+    {
+        switch (CharacterInformation.character)
+        {
+            case CharacterInformation.Character.Grandma:
+                chosenCharacterPrefab = grandmaPrefab;
+                break;
+            case CharacterInformation.Character.Student:
+                chosenCharacterPrefab = studentPrefab;
+                break;
+        }
+    }
+}
