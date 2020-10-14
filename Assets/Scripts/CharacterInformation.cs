34d96f5ec4ed99247b5da61a8fc7f713c1cd3161
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2020-10-14 21:03:04 +0200

    add dropdown to choose character

---
+++ Assets/Scripts/CharacterInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CharacterInformation
+{
+    public static Character character = Character.Student;
+
+    public enum Character
+    {
+        Grandma, Student
+    }
+
+}
---
+++ Assets/Scripts/DropdownHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownHandler : MonoBehaviour
+{
+
+    void Start()
+    {
+        var dropdown = transform.GetComponent<Dropdown>();
+
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+    }
+
+    void DropdownItemSelected(Dropdown dropdown)
+    {
+        switch(dropdown.value)
+        {
+            case 0:
+                CharacterInformation.character = CharacterInformation.Character.Student;
+                break;
+            case 1:
+                CharacterInformation.character = CharacterInformation.Character.Grandma;
+                break;
+        }
+    }
+}
