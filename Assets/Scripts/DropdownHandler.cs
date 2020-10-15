7d538dcc916084965c408054dadbc10db639d196
Magdalena Mayerhofer <m.mayerhofer@student.tugraz.at>
2020-10-15 22:42:10 +0200

    make menu nice looking

--- Assets/Scripts/DropdownHandler.cs
+++ Assets/Scripts/DropdownHandler.cs
@@ -4,9 +4,14 @@ using UnityEngine.UI;
 public class DropdownHandler : MonoBehaviour
 {
 
+    public Image playerGraphic;
+    public Sprite grandma;
+    public Sprite student;
+
     void Start()
     {
         CharacterInformation.character = CharacterInformation.Character.Student;
+        playerGraphic.overrideSprite = student;
 
         var dropdown = transform.GetComponent<Dropdown>();
 
@@ -19,9 +24,11 @@ public class DropdownHandler : MonoBehaviour
         {
             case 0:
                 CharacterInformation.character = CharacterInformation.Character.Student;
+                playerGraphic.overrideSprite = student;
                 break;
             case 1:
                 CharacterInformation.character = CharacterInformation.Character.Grandma;
+                playerGraphic.overrideSprite = grandma;
                 break;
         }
     }
